cd71683dbfa1227eafe86f0231225afc3102c292
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-02 12:34:24 -0600

    📅  | Thu Jun  2 12:34:24 MDT 2022

--- nest-recipes/src/app.module.ts
+++ nest-recipes/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipesModule } from './recipes/recipes.module';
+import connection from './typeorm-config';
 
 @Module({
-  imports: [RecipesModule],
+  imports: [RecipesModule, TypeOrmModule.forRoot(connection)],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-recipes/src/typeorm-config.ts
+++ nest-recipes/src/typeorm-config.ts
@@ -1,1 +1,24 @@
-export class TypeormConfig {}
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export class TypeormConnection {
+  getConfig() {
+    let ormConfig: TypeOrmModuleOptions;
+    if (process.env.PRODUCTION) {
+      throw new Error('configure Production server');
+    } else {
+      ormConfig = {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'pass123',
+        database: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+      };
+    }
+    return ormConfig;
+  }
+}
+
+export default new TypeormConnection().getConfig();
