1209496a33b4412b6c49ca13301ad6ba26f86dc1
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-03 11:20:56 -0600

    📅  | Fri Jun  3 11:20:56 MDT 2022

--- ng-recipes/src/app/app.module.ts
+++ ng-recipes/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatCardModule } from '@angular/material/card';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,10 +8,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DisplayCardsComponent } from './display-cards/display-cards.component';
 import { RecipeCardComponent } from './display-cards/recipe-card/recipe-card.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-  declarations: [AppComponent, DisplayCardsComponent, RecipeCardComponent],
+  declarations: [AppComponent, DisplayCardsComponent, RecipeCardComponent, HeroComponent],
   imports: [
+    MatCardModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
