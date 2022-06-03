c1de9302d9a87840d638d9a22afdf0961f4afc45
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-03 12:00:12 -0600

    📅  | Fri Jun  3 12:00:12 MDT 2022

--- ng-recipes/src/app/display-cards/recipe-card/recipe-card.component.ts
+++ ng-recipes/src/app/display-cards/recipe-card/recipe-card.component.ts
@@ -9,16 +9,21 @@ import { Recipe } from 'src/app/core/models/recipe';
 export class RecipeCardComponent implements OnInit {
   recipe: Recipe = {
     id: 1,
-    title: 'string', // ok
-    ingredients: 'string', // ok
-    tags: 'string',
-    imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg', // ok
+    title: 'Hot wings', // ok
+    ingredients: 'Chicken Wings, Cayene Peper, Hot Sauce(ex Tabisco), Butter.', // ok
+    tags: 'meat spicy chicken gluten-free',
+    imageUrl:
+      'https://www.denverpost.com/wp-content/uploads/2017/07/kaya-websize-53.jpg', // ok
     cookingTime: 3, //ok
     prepTime: 1,
     yield: 1,
     steps: 'string',
     rating: 1,
   };
+
+  tags: string[] | undefined = this.recipe.tags
+    ? this.recipe.tags.split(' ')
+    : undefined;
 
   constructor() {}
 
