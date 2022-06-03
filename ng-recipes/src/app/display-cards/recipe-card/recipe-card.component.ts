965316c1921d641861202891917b62dbd93541f2
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-03 11:20:49 -0600

    📅  | Fri Jun  3 11:20:49 MDT 2022

--- ng-recipes/src/app/display-cards/recipe-card/recipe-card.component.ts
+++ ng-recipes/src/app/display-cards/recipe-card/recipe-card.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/core/models/recipe';
 
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
-  styleUrls: ['./recipe-card.component.scss']
+  styleUrls: ['./recipe-card.component.scss'],
 })
 export class RecipeCardComponent implements OnInit {
+  recipe: Recipe = {
+    id: 1,
+    title: 'string', // ok
+    ingredients: 'string', //
+    tags: 'string',
+    imageUrl: 'string',
+    cookingTime: 3,
+    prepTime: 1,
+    yield: 1,
+    steps: 'string',
+    rating: 1,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
